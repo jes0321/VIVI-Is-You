26f850f572d71982e73f41cce1e289b7af9d587e
jes0321 <jesjes0321@naver.com>
2024-11-06 00:15:40 +0900

    IVerbable

---
+++ Assets/01.Scripts/JES/Object/IVerbable.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+public interface IVerbable
+{
+    public void VerbApply(List<Agent> agents);
+}
--- Assets/01.Scripts/JES/Object/Subject.cs
+++ Assets/01.Scripts/JES/Object/Subject.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using UnityEditor.Analytics;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
-public class Subject : Object
+public class Subject : Object, IVerbable
 {
-    [SerializeField] private AgentType _agentType;
+    [SerializeField] private AgentDataSO _agentData;
     private List<Agent> _agents = new List<Agent>();//자신이 조종하는 Agent 리스트
-
     protected override void Awake()
     {
         base.Awake();
 
         FindObjectsByType<Agent>(FindObjectsSortMode.None).ToList().ForEach(agent =>
         {
-            if (_agentType == agent.AgentType) _agents.Add(agent);
+            if (_agentData._type == agent.AgentType) _agents.Add(agent);
         });
     }
     public List<Agent> GetAgents()
     {
         return _agents;
+    }
+
+    public void VerbApply(List<Agent> agents)
+    {
+        agents.ForEach(agent =>
+        {
+            agent.UpdateData(_agentData);
+        });
+        agents = new List<Agent>();
     }
 }
