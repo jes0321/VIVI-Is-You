d064ffcc492d17113bb66279914dbb02ecbe65b8
Parksungje <Parksungje0213@gmail.com>
2024-11-08 01:49:15 +0900

    StageStart

---
+++ Assets/01.Scripts/PSJ/Stage/StageBt.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StageBt : MonoBehaviour
+{
+    [SerializeField] private ScriptableObject _stageSO;
+
+    private void Start(StageSO stageSO)
+    {
+
+    }
+}
---
+++ Assets/01.Scripts/PSJ/Stage/StageSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="SO/Stage")]
+public class StageSO : ScriptableObject
+{
+    public int currentStage;
+}
