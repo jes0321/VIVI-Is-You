0d29a7bc4a1f7a0ff9affcbbb9e2d2c48720329f
Parksungje <Parksungje0213@gmail.com>
2024-11-24 22:16:44 +0900

    fix : win text

--- Assets/01.Scripts/PSJ/WinAction.cs
+++ Assets/01.Scripts/PSJ/WinAction.cs
@@ -1,23 +1,43 @@
+using DG.Tweening;
+using Mono.Cecil;
 using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WinAction : MonoSingleton<WinAction>
 {
-    [SerializeField] private GameObject _mapGrid;
-    [SerializeField] private TextMeshProUGUI _winText;
+    [SerializeField] private Image _fadeImage;
+    [SerializeField] private float _fadeDuration = 1f;
+    private readonly int _valueHash = Shader.PropertyToID("_Value");
+    [SerializeField] private GameObject _winText;
 
-    public void WinText()
+    private void Awake()
     {
-        _mapGrid.SetActive(false);
-        _winText.enabled = true;
-        StartCoroutine(WaitText());
+        _fadeImage.material = new Material(_fadeImage.material);
+
+        HandleFadeEvent(true);
     }
-    
-    IEnumerator WaitText()
+
+
+    public void HandleFadeEvent(bool isFadeIn)
     {
-        yield return new WaitForSeconds(2f);
-        SceneManager.LoadScene(SceneName.LobbyScene);
+        float fadeValue = isFadeIn ? 1.2f : 0f;
+        float startValue = isFadeIn ? 0f : 1.2f;
+
+        _fadeImage.material.SetFloat(_valueHash, startValue);
+
+
+
+        var tweenCore = _fadeImage.material.DOFloat(fadeValue, _valueHash, _fadeDuration);
+
+        if (isFadeIn == false)
+        {
+            tweenCore.OnComplete(() => {
+                var tween = _winText.transform.DOScale(new Vector3(0.17f,0.3f,0), 1);
+                tween.OnComplete(() => SceneManager.LoadScene(SceneName.LobbyScene));
+            });
+        }
     }
 }
