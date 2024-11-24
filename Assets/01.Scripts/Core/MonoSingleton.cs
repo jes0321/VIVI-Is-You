d0606d34869ea3edd7f61e38c8e121f97cc85dde
jes0321 <jesjes0321@naver.com>
2024-11-24 21:53:47 +0900

    fix : mono

--- Assets/01.Scripts/Core/MonoSingleton.cs
+++ Assets/01.Scripts/Core/MonoSingleton.cs
@@ -15,7 +15,7 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
 
             if (_instance == null)
             {
-                _instance =  FindAnyObjectByType<T>();
+                _instance =  FindFirstObjectByType<T>();
                 if (_instance == null)
                     Debug.LogError($"{typeof(T).Name} singleton is not exist");
                 else
